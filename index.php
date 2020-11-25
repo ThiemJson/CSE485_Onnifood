a148bbe21a30dfa976b8e86dfdd95a820c39efe2
Nguyen Cao Thiem <nguyen_thiem@teneocto.io>
2020-11-25 16:39:27 +0700

    Init git

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&display=swap" rel="stylesheet">
+    <title>Food order</title>
+</head>
+
+<body>
+    <header class="header">
+        <div class="hero-text-box">
+            <h1>Goodbye junk food. <br> Hello heathy food.</h1>
+            <a href="#" class="btn btn-full">I'm hungry</a>
+            <a href="" class="btn btn-ghost">Show me</a>
+        </div>
+    </header>
+    <section class="section"></section>
+    <footer class="footer"></footer>
+</body>
+
+</html>
