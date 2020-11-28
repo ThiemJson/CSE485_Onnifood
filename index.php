b7daa9a374964b6185f50a2683d29c33c2d5454c
Nguyen Cao Thiem <nguyen_thiem@teneocto.io>
2020-11-28 15:25:12 +0700

    Impement how ti work

--- index.php
+++ index.php
@@ -40,25 +40,25 @@
         </div>
         <div class="row">
             <div class="col span-1-of-4 box">
-                <ion-icon name="infinite-outline" class ="icon-big"></ion-icon>
+                <ion-icon name="infinite-outline" class="icon-big"></ion-icon>
                 <h3>Up to 365 days/year</h3>
                 <p>Never cook again! We really mean that. Our subscription plans include up to 365 days/year coverage. You can also choose to order more flexibly if that's your style.
                 </p>
             </div>
             <div class="col span-1-of-4 box">
-                <ion-icon name="stopwatch-outline" class ="icon-big"></ion-icon>
+                <ion-icon name="stopwatch-outline" class="icon-big"></ion-icon>
                 <h3>Ready in 20 minutes</h3>
                 <p>You're only twenty minutes away from your delicious and super healthy meals delivered right to your home. We work with the best chefs in each town to ensure that you're 100% happy.
                 </p>
             </div>
             <div class="col span-1-of-4 box">
-                <ion-icon name="nutrition-outline" class ="icon-big"></ion-icon>
+                <ion-icon name="nutrition-outline" class="icon-big"></ion-icon>
                 <h3>100% organic</h3>
                 <p>All our vegetables are fresh, organic and local. Animals are raised without added hormones or antibiotics. Good for your health, the environment, and it also tastes better!
                 </p>
             </div>
             <div class="col span-1-of-4 box">
-                <ion-icon name="cart-outline" class ="icon-big"></ion-icon>
+                <ion-icon name="cart-outline" class="icon-big"></ion-icon>
                 <h3>Order anything</h3>
                 <p>We don't limit your creativity, which means you can order whatever you feel like. You can also choose from our menu containing over 100 delicious meals. It's up to you!
                 </p>
@@ -67,50 +67,77 @@
     </section>
 
     <section class="section-meals">
-        <ul class="meals-showcase">
+        <ul class="meals-showcase clearfix">
             <li>
-                <figure class ="meal-photo" >
+                <figure class="meal-photo">
                     <img src="images/1.jpg" alt="Korean bibimbap with egg and vegetables">
                 </figure>
             </li>
             <li>
-                <figure class ="meal-photo" >
+                <figure class="meal-photo">
                     <img src="images/2.jpg" alt="Simple italian pizza with cherry tomatoes">
                 </figure>
             </li>
             <li>
-                <figure class ="meal-photo" >
+                <figure class="meal-photo">
                     <img src="images/3.jpg" alt="Chicken breast steak with vegetables">
                 </figure>
             </li>
             <li>
-                <figure class ="meal-photo" >
+                <figure class="meal-photo">
                     <img src="images/4.jpg" alt="Autumn pumpkin soup">
                 </figure>
             </li>
         </ul>
-        <ul class="meals-showcase">
+        <ul class="meals-showcase clearfix">
             <li>
-                <figure class ="meal-photo" >
+                <figure class="meal-photo">
                     <img src="images/5.jpg" alt="Paleo beef steak with vegetables">
                 </figure>
             </li>
             <li>
-                <figure class ="meal-photo" >
+                <figure class="meal-photo">
                     <img src="images/6.jpg" alt="Healthy baguette with egg and vegetables">
                 </figure>
             </li>
             <li>
-                <figure class ="meal-photo" >
+                <figure class="meal-photo">
                     <img src="images/7.jpg" alt="Burger with cheddar and bacon">
                 </figure>
             </li>
             <li>
-                <figure class ="meal-photo" >
+                <figure class="meal-photo">
                     <img src="images/8.jpg" alt="Granola with cherries and strawberries">
                 </figure>
             </li>
         </ul>
+    </section>
+
+    <section class="section-steps">
+        <div class="row">
+            <h2> How it works &mdash; Simple as 1, 2, 3</h2>
+        </div>
+        <div class="row">
+            <div class="col span-1-of-2 steps-box">
+                <img src="images/app-iPhone.png" alt="App Iphone" class="app-iphone">
+            </div>
+            <div class="col span-1-of-2 steps-box">
+                <div class="work-step">
+                    <div>1</div>
+                    <p>Choose the subscription plan that best fits your needs and sign up today.</p>
+                </div>
+                <div class="work-step">
+                    <div>2</div>
+                    <p>Order your delicious meal using our mobile app or website. Or you can even call us!</p>
+                </div>
+                <div class="work-step">
+                    <div>3</div>
+                    <p>Enjoy your meal after less than 20 minutes. See you the next time!</p>
+                </div>
+                <a href="#" class="btn-app"><img src="images/download-app-android.png" alt="download-app-android"></a>
+                <a href="#" class="btn-app"><img src="images/download-app.svg" alt="download-app-iphone"></a>
+            </div>
+        </div>
     </section>
     <footer class="footer"></footer>
 
@@ -120,14 +147,13 @@
 
 
 <!-- 
-    1. Korean bibimbap with egg and vegetables
-    2. Simple italian pizza with cherry tomatoes 
-    3. Chicken breast steak with vegetables  
-    4. Autumn pumpkin soup
-    5. Paleo beef steak with vegetables
-    6. Healthy baguette with egg and vegetables
-    7. Burger with cheddar and bacon 
-    8. Granola with cherries and strawberries
+   Section 3: How it works
+Title: How it works - Simple as 1, 2, 3
+
+    1. Choose the subscription plan that best fits your needs and sign up today.
+    2. Order your delicious meal using our mobile app or website. Or you can even call us!
+    3. Enjoy your meal after less than 20 minutes. See you the next time!
+
 
 (see image numbers in the assets folder)
  -->
